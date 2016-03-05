1cf9961e8ea035f7fd424c53d3e9b57384c41a50
bradc87 <brad87@gmail.com>
2016-03-05 11:22:01 -0500

    Initial Upload

---
+++ dataPhobe/Helpers/dPClassLib.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dataPhobe.Helpers
+{
+    public class dPEngine
+    {
+      public double getCompIntAnnual(double princBalance, double intRate, int compoundInterval, int investmentDuration)
+        {
+            double P = princBalance;
+            double r = intRate;
+            int n = compoundInterval;
+            int t = investmentDuration;
+
+            double rn = r / n + 1;
+            double nt = n * t;
+            double rnn = Math.Pow(rn, nt);
+            double A = P * rnn;
+            return A;
+        }
+
+
+    }
+}
