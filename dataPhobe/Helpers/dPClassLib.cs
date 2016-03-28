705181b3fa6848cbcbb599cd6a137dd47ee0dc69
bradc87 <brad87@gmail.com>
2016-03-28 19:18:01 -0400

    nightly

--- dataPhobe/Helpers/dPClassLib.cs
+++ dataPhobe/Helpers/dPClassLib.cs
@@ -1,13 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Web;
 
 namespace dataPhobe.Helpers
 {
     public class dPEngine
     {
-      public double getCompIntAnnual(double princBalance, double intRate, int compoundInterval, int investmentDuration)
+
+        public DataTable getCompIntWithContributionsLedger(double monthlyPmt, double princBalance, double intRate, int contribInterval, int compoundInterval, int investmentDuration)
+        {
+            double P = princBalance;
+            double r = intRate;
+            int n = compoundInterval;
+            int t = investmentDuration;
+            int k = contribInterval;
+            double pmt = monthlyPmt;
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("YearNum");
+            dt.Columns.Add("Balance");
+
+
+            for (int i = 1; i <= t; i++)
+            {
+
+            }
+
+        }
+
+        public double getCompIntWithContributions(double monthlyPmt, double princBalance, double intRate, int contribInterval, int compoundInterval, int investmentDuration)
+        {
+
+            double P = princBalance;
+            double r = intRate;
+            int n = compoundInterval;
+            int t = investmentDuration;
+            int k = contribInterval;
+            double pmt = monthlyPmt;
+
+
+            double A = getCompPrincipal(P, r, n, t) + getFVSeries(pmt, r, k, t);
+
+            return A;
+
+        }
+
+        public double getCompPrincipal(double princBalance, double intRate, int compoundInterval, int investmentDuration)
         {
 	    // Global Viarible Declaration
             double P = princBalance;
@@ -15,10 +55,31 @@ namespace dataPhobe.Helpers
             int n = compoundInterval;
             int t = investmentDuration;
 
-            double rn = r / n + 1;
+            double rn = r / n;
+            double rn1 = rn + 1;
             double nt = n * t;
-            double rnn = Math.Pow(rn, nt);
+            double rnn = Math.Pow(rn1, nt);
             double A = P * rnn;
+            return A;
+        }
+
+        private double getFVSeries(double monthlyPmt, double intRate, int contribInterval, int investmentDuration)
+        {
+
+            double pmt = monthlyPmt;
+            double r = intRate;
+            int n = contribInterval;
+            int t = investmentDuration;
+
+            //get future value of series 
+            double nt = n * t;
+            double rn = r / n;
+            double rn1 = rn + 1;
+            double rn1nt = Math.Pow(rn1, nt);
+            double rnnt = rn1nt - 1;
+            double a = rnnt / rn;
+            double A = pmt * a;
+
             return A;
         }
 
