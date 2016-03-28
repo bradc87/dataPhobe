ccc1fe6df80aae554451a7b415e76f7601804212
bradc87 <brad87@gmail.com>
2016-03-28 18:20:05 -0400

    nightly

---
+++ dataPhobe/Controllers/FinancialController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dataPhobe.Controllers
+{
+    public class FinancialController : Controller
+    {
+        // GET: Financial
+        public ActionResult CompInterest()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CompInterest(double princBalance, double intRate, int compoundInterval, int investmentDuration)
+        {
+
+            dataPhobe.Helpers.dPEngine engine = new Helpers.dPEngine();
+            double compInterestResult = engine.getCompIntAnnual(princBalance, intRate, compoundInterval, investmentDuration);
+            ViewBag.CIResult = compInterestResult.ToString();
+            return View();
+        }
+    }
+}
