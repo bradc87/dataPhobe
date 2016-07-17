6a9c5c25aaa2a6d130d082f2297009e3b8a688ae
bradc87 <brad87@gmail.com>
2016-07-17 02:12:00 -0400

    Yup

--- dataPhobe/Controllers/FinancialController.cs
+++ dataPhobe/Controllers/FinancialController.cs
@@ -21,10 +21,13 @@ namespace dataPhobe.Controllers
         [HttpPost]
         public ActionResult CompInterest(double princBalance, double intRate, int compoundInterval, int investmentDuration, double contribAmt)
         {
-
+            double decimalIntRate;
+            decimalIntRate = intRate / 100;
             dataPhobe.Helpers.dPEngine engine = new Helpers.dPEngine();
-            DataTable dt = engine.getCompIntWithContributionsLedger(contribAmt, princBalance, intRate, 12,compoundInterval,investmentDuration);
-            DataTable dTab = new DataTable();
+            DataTable dt = engine.getCompIntWithContributionsLedger(contribAmt, princBalance, decimalIntRate, 12,compoundInterval,investmentDuration);
+            Double investmentTotal = engine.getCompIntWithContributions(contribAmt, princBalance, decimalIntRate, 12, compoundInterval, investmentDuration);
+            //DataTable dTab = new DataTable();
+            ViewBag.summaryText = string.Format ("With a principal balance of <b> {0} </b> and monthly contributions of <b>{1}</b> at an interest rate of <b>{2}%</b> your balance would be <b>{3}</b>", princBalance, contribAmt, intRate, investmentTotal);
             ViewBag.chartData = genData(dt);
             return View();
         }
